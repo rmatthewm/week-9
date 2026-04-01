1cf042bf0453001588e8cdb6fa17783204a45d89
Raul Matthew Mosley <raulmatthewmosley@gmail.com>
2026-03-31 20:15:54 -0400

    finished part 1

--- apputil.py
+++ apputil.py
@@ -3,6 +3,14 @@ import pandas as pd
 
 def GroupEstimate(object):
     def __init__(self, estimate):
+        """ Constructor
+
+        Args:
+            estimate (str): either 'mean' or 'median' 
+        """
+        if estimate != 'mean' and estimate != 'median':
+            raise ValueError('Estimate must be either mean or median.')
+
         self.estimate = estimate
     
     def fit(self, X, y):
