6e79e33faf8af74a9e06a4b2b5b4e2bd0a6c8a49
Raul Matthew Mosley <raulmatthewmosley@gmail.com>
2026-03-31 21:59:01 -0400

    finished part 3

--- apputil.py
+++ apputil.py
@@ -14,6 +14,12 @@ class GroupEstimate(object):
         self.estimate = estimate
     
     def fit(self, X, y):
+        """ Adds the data in y as a column in X
+
+        Args:
+            X (pandas.DataFrame): a dataframe with the data to analyze
+            y (array-like): the data to fit to X
+        """
 
         # Make a copy of the dataframe
         X = X.copy()
@@ -21,13 +27,8 @@ class GroupEstimate(object):
         # Convert y to a pandas series 
         y = pd.Series(y)
 
-        # y must not be missing any values
-        #if y.isna().count() > 0:
-        #    raise ValueError('Array y must not be missing any values.')
-
         # Get the column names in X before adding y
         X_columns = list(X.columns)
-        print(X_columns)
 
         # Combine the data together
         X['y'] = y
@@ -36,7 +37,36 @@ class GroupEstimate(object):
         self.X_grouped = X.groupby(X_columns, as_index=False).agg(self.estimate)
 
     def predict(self, X):
-        return None
+        """ Returns the predicted value for various categories given in X
+
+        Args:
+            X (list(list)): a 2D list of categories matching the columns in the original X 
+
+        Returns:
+            list: the predicted values for each entry in X 
+        """
+        # The list to add the estimates to that we will return
+        estimates = []
+
+        # Since we don't know what the columns are in X, we can filter by one
+        # column at a time
+        X_columns = self.X_grouped.columns
+        for category in X:
+            filter = self.X_grouped
+            for i in range(len(category)):
+                filter = filter.loc[self.X_grouped[X_columns[i]] == category[i]]
+
+            # If the filtered dataframe is empty, add NaN to the list
+            if filter.empty:
+                estimates.append(pd.NA)
+
+            # Otherwise, add the y value
+            else:
+                # Get the individual y value out of the series
+                # (Converting to float to match example in exercises)
+                estimates.append(float(filter['y'].iloc[0]))
+
+        return estimates
 
 
 # For testing
